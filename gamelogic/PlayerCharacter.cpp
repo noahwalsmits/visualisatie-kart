43b72e915223fbaba370deee2dfc12b7a4b0bd50
noah <noahwalsmits@telfort.nl>
2023-04-18 17:24:06 +0200

    added forwards/backwards driving

--- gamelogic/PlayerCharacter.cpp
+++ gamelogic/PlayerCharacter.cpp
@@ -1,23 +1,60 @@
 #include "PlayerCharacter.h"
+#include <algorithm>
 
+//TODO change to use callbacks instead
 void PlayerCharacter::update(float deltaTime, GLFWwindow* window)
 {
-	//TODO update position values
-	float speed = 2.0f * deltaTime;
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) //up
+	//driving
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) //forwards
 	{
-		this->driverModel.position.z -= speed;
+		if (this->speed < 0.0f) //brake
+		{
+			this->speed += BRAKING_DECCELERATION * deltaTime;
+			this->speed = std::min(this->speed, 0.0f);
+		}
+		else //drive forwards
+		{
+			this->speed += FORWARD_ACCELERATION * deltaTime;
+			this->speed = std::min(this->speed, MAX_FORWARD_SPEED);
+		}
 	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) //down
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) //backwards
 	{
-		this->driverModel.position.z += speed;
+		if (this->speed > 0.0f) //brake
+		{
+			this->speed -= BRAKING_DECCELERATION * deltaTime;
+			this->speed = std::max(this->speed, 0.0f);
+		}
+		else //drive backwards
+		{
+			this->speed += REVERSE_ACCELERATION * deltaTime;
+			this->speed = std::max(this->speed, MAX_REVERSE_SPEED);
+		}
 	}
+	if ((glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE) && (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_RELEASE)) //idle
+	{
+		if (this->speed > 0.0f) //slow to a stop
+		{
+			this->speed -= IDLE_DECCELERATION * deltaTime;
+			this->speed = std::max(this->speed, 0.0f);
+		}
+		if (this->speed < 0.0f) //slow to a stop
+		{
+			this->speed += IDLE_DECCELERATION * deltaTime;
+			this->speed = std::min(this->speed, 0.0f);
+		}
+	}
+
+	//steering
 	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) //left
 	{
-		this->driverModel.position.x -= speed;
+		//this->driverModel.position.x -= speed;
 	}
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) //right
 	{
-		this->driverModel.position.x += speed;
+		//this->driverModel.position.x += speed;
 	}
+
+	//apply speed
+	this->driverModel.position.x += this->speed; //TODO change to z position after adding steering
 }
--- gamelogic/PlayerCharacter.h
+++ gamelogic/PlayerCharacter.h
@@ -17,5 +17,21 @@ private:
 	//AnimatedModel carModel;
 	AnimatedModel driverModel = AnimatedModel("assets/animated_yoshi/yoshi.dae",
 		{ "assets/animated_yoshi/yoshiLeft.dae", "assets/animated_yoshi/yoshiRight.dae" });
+
+	//speed that is added per second while going forwards
+	static constexpr float FORWARD_ACCELERATION = 0.1f;
+	//speed that is added per second while going backwards
+	static constexpr float REVERSE_ACCELERATION = -0.05f;
+	//maximum speed that can be reached while going forwards
+	static constexpr float MAX_FORWARD_SPEED = 0.2f;
+	//maximum speed that can be reached while going backwards
+	static constexpr float MAX_REVERSE_SPEED = -0.1f;
+	//speed change per second while reversing with positive speed or going forward with negative speed
+	static constexpr float BRAKING_DECCELERATION = 0.3f;
+	//speed change towards 0 per second while not going forward or reversing
+	static constexpr float IDLE_DECCELERATION = 0.2f;
+
+	//the current forward positional change per second
+	float speed = 0.0f;
 };
 
