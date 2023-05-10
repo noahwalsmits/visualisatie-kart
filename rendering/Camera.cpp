a98c71c36aac3bf5eb7c955267ebd6ebe2f0a2b3
noah <noahwalsmits@telfort.nl>
2023-05-10 15:58:48 +0200

    camera is now orbital

--- rendering/Camera.cpp
+++ rendering/Camera.cpp
@@ -8,13 +8,13 @@ Camera::Camera()
 
 glm::mat4 Camera::getViewMatrix()
 {
-	//return glm::lookAt(this->position, this->target, this->up); //orbits the target
-	return glm::lookAt(this->position, this->position + this->front, this->up);
+	return glm::lookAt(this->position, this->target, this->worldUp);
 }
 
 void Camera::mouseMoved(double deltaX, double deltaY)
 {
-	this->yaw += deltaX * this->MOUSE_SENSITIVITY;
+	this->yaw -= deltaX * this->MOUSE_SENSITIVITY;
+	this->yaw = remainderf(this->yaw, 360.0f);
 	this->pitch -= deltaY * this->MOUSE_SENSITIVITY;
 	if (pitch > 89.0f)
 	{
@@ -29,32 +29,12 @@ void Camera::mouseMoved(double deltaX, double deltaY)
 
 void Camera::pollMovementKeys(float deltaTime, GLFWwindow* window)
 {
-	float cameraSpeed = this->MOVEMENT_SPEED * deltaTime;
-	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
-		cameraSpeed *= 10.0f;
-
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		this->position.z -= cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		this->position.z += cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		this->position.x -= cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		this->position.x += cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
-		this->position.y -= cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-		this->position.y += cameraSpeed;
+	
 }
 
 void Camera::updateViewingAngle()
 {
-	glm::vec3 direction = glm::vec3(0.0f, 0.0f, 0.0f);
-	direction.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
-	direction.y = sin(glm::radians(pitch));
-	direction.z = sin(glm::radians(this->yaw) * cos(glm::radians(this->pitch)));
-	this->front = glm::normalize(direction);
-
-	this->right = glm::normalize(glm::cross(this->front, this->worldUp));
-	this->up = glm::normalize(glm::cross(this->right, this->front));
+	this->position.x = this->target.x + this->distance * cos(glm::radians(this->pitch)) * cos(glm::radians(this->yaw));
+	this->position.y = this->target.y + this->distance * sin(glm::radians(this->pitch));
+	this->position.z = this->target.z + this->distance * cos(glm::radians(this->pitch)) * sin(glm::radians(this->yaw));
 }
--- rendering/Camera.h
+++ rendering/Camera.h
@@ -14,13 +14,10 @@ private:
 	static constexpr float MOVEMENT_SPEED = 3.0f;
 	static constexpr float MOUSE_SENSITIVITY = 0.1f;
 
-	glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f); //TODO stay close to the player
-	glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
-	glm::vec3 right = glm::vec3(0.0f, 0.0f, -1.0f);
-	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f);
 	glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
-	//glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f); //TODO target the player
-
+	float distance = 3.0f;
 	float yaw = -90.0f;
 	float pitch = 0.0f;
 
