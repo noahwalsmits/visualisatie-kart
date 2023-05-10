f3dc753e6acbd894e9a5022c7b3d4d6745e59929
noah <noahwalsmits@telfort.nl>
2023-05-10 15:21:22 +0200

    made camera position private

--- rendering/Camera.h
+++ rendering/Camera.h
@@ -5,8 +5,6 @@
 class Camera
 {
 public:
-	glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f); //TODO stay close to the player
-
 	Camera();
 	glm::mat4 getViewMatrix();
 	void mouseMoved(double deltaX, double deltaY);
@@ -16,10 +14,11 @@ private:
 	static constexpr float MOVEMENT_SPEED = 3.0f;
 	static constexpr float MOUSE_SENSITIVITY = 0.1f;
 
-	glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f); //TODO stay close to the player
 	glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 right = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
 	//glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f); //TODO target the player
 
 	float yaw = -90.0f;
