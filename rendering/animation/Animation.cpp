2e1161e243cf97341c7de9468bbc924e24cd2dee
noah <noahwalsmits@telfort.nl>
2023-05-16 21:45:16 +0200

    cleaned up animation

--- rendering/animation/Animation.cpp
+++ rendering/animation/Animation.cpp
@@ -1,1 +1,72 @@
 #include "Animation.h"
+#include <iostream>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+
+Animation::Animation(const std::string& animationPath, Model* model)
+{
+    std::cout << "\tLoading animation: " << animationPath << std::endl;
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(animationPath, aiProcess_Triangulate);
+    assert(scene && scene->mRootNode);
+    assert(scene->mNumAnimations > 0);
+    auto animation = scene->mAnimations[0];
+    m_Duration = animation->mDuration;
+    m_TicksPerSecond = animation->mTicksPerSecond;
+    ReadHeirarchyData(m_RootNode, scene->mRootNode);
+    ReadMissingBones(animation, *model);
+}
+
+Bone* Animation::FindBone(const std::string& name)
+{
+    auto iter = std::find_if(m_Bones.begin(), m_Bones.end(),
+        [&](const Bone& Bone)
+        {
+            return Bone.GetBoneName() == name;
+        }
+    );
+    if (iter == m_Bones.end()) return nullptr;
+    else return &(*iter);
+}
+
+void Animation::ReadMissingBones(const aiAnimation* animation, Model& model)
+{
+    int size = animation->mNumChannels;
+
+    auto& boneInfoMap = model.getBoneInfoMap();//getting m_BoneInfoMap from Model class
+    int& boneCount = model.getBoneCount(); //getting the m_BoneCounter from Model class
+
+    //reading channels(bones engaged in an animation and their keyframes)
+    for (int i = 0; i < size; i++)
+    {
+        auto channel = animation->mChannels[i];
+        std::string boneName = channel->mNodeName.data;
+
+        if (boneInfoMap.find(boneName) == boneInfoMap.end())
+        {
+            boneInfoMap[boneName].id = boneCount;
+            boneCount++;
+        }
+        m_Bones.push_back(Bone(channel->mNodeName.data,
+            boneInfoMap[channel->mNodeName.data].id, channel));
+    }
+
+    m_BoneInfoMap = boneInfoMap;
+}
+
+void Animation::ReadHeirarchyData(AssimpNodeData& dest, const aiNode* src)
+{
+    assert(src);
+
+    dest.name = src->mName.data;
+    dest.transformation = AssimpGLMHelpers::ConvertMatrixToGLMFormat(src->mTransformation);
+    dest.childrenCount = src->mNumChildren;
+
+    for (int i = 0; i < src->mNumChildren; i++)
+    {
+        AssimpNodeData newData;
+        ReadHeirarchyData(newData, src->mChildren[i]);
+        dest.children.push_back(newData);
+    }
+}
